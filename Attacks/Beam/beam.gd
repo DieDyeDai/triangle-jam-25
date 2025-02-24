d7acc8f57f4d0c9ee75ba8c8b9541930eef2a434
DieDyeDai <mzdai@ad.unc.edu>
2025-02-24 06:08:37 -0500

    beam sprite improvement, beam draw bugfix

--- Attacks/Beam/beam.gd
+++ Attacks/Beam/beam.gd
@@ -34,9 +34,9 @@ func initialize(pos: Vector2i) -> void:
 		next_warning_tile.y = 1
 	
 	var y = base_position.y
-	while (y < Globals.Y_UPPER and y > Globals.Y_LOWER):
-		y += dir
+	while (y <= Globals.Y_UPPER and y >= Globals.Y_LOWER):
 		beam_tiles_to_draw.append(Vector2i(base_position.x, y))
+		y += dir
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -54,20 +54,19 @@ func fire() -> void:
 	for tile : Vector2i in warning_positions:
 		positions.append(tile)
 	
-	print(base_position)
+	#print(base_position)
 	
 	line_section_1.add_point(Globals.get_global_position(base_position) + Vector2(0, -0.5 * Globals.TILE_HEIGHT))
 	line_section_1.add_point(Vector2(Globals.get_global_position(base_position).x, -3 * Globals.TILE_HEIGHT))
 
-	$Line2D.add_point(Globals.get_global_position(base_position))
-	$Line2D.add_point(Vector2(Globals.get_global_position(Vector2i(base_position)).x, -3 * Globals.TILE_HEIGHT))
-
-	print(line_section_1.points)
+	#print(line_section_1.points)
+	
+	print(beam_tiles_to_draw)
 	
 	line_section_2.add_point(Vector2(Globals.get_global_position(beam_tiles_to_draw[-5])) - Vector2(0, Globals.TILE_HEIGHT))
 	line_section_2.add_point(Vector2(Globals.get_global_position(beam_tiles_to_draw[-1])) + Vector2(0, Globals.TILE_HEIGHT * 4))
 	
-	print(line_section_2.points)
+	#print(line_section_2.points)
 	
 	beam_width_tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	beam_width_tween.tween_property(line_section_1, "width", 32, 0.1)
