077959372fc8515e05b630f0277e295d4bcae1d8
DieDyeDai <mzdai@ad.unc.edu>
2025-02-28 14:57:03 -0500

    counter white flash

--- UI/diamond_count.gd
+++ UI/diamond_count.gd
@@ -5,6 +5,7 @@ const UIE_COUNT_RED = preload("res://Assets/UIECountRed.png")
 const UIE_COUNT_BLUE = preload("res://Assets/UIECountBlue.png")
 
 var flash_white_timer : Timer = null
+var flash_white_timer2 : Timer = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,6 +14,12 @@ func _ready() -> void:
 	flash_white_timer.set_wait_time(0.1)
 	add_child(flash_white_timer)
 	flash_white_timer.timeout.connect(self.on_flash_white_end)
+
+	flash_white_timer2 = Timer.new()
+	flash_white_timer2.set_one_shot(false)
+	flash_white_timer2.set_wait_time(0.1)
+	add_child(flash_white_timer2)
+	flash_white_timer2.timeout.connect(self.on_flash_white_end)
 
 func initialize(isP1: bool, isP2: bool):
 	if isP1:
@@ -23,17 +30,30 @@ func initialize(isP1: bool, isP2: bool):
 
 func update(count: int):
 	count = clampi(count, 0, UIEnergyBar.MAX_I)
-	if frame != count:
+	if frame < count:
 		frame = count
-		flash_white()
+		material.set("shader_parameter/enabled", true)
+		await get_tree().create_timer(0.05, false).timeout
+		material.set("shader_parameter/enabled", false)
+		await get_tree().create_timer(0.03, false).timeout
+		material.set("shader_parameter/enabled", true)
+		await get_tree().create_timer(0.05, false).timeout
+		material.set("shader_parameter/enabled", false)
+	elif frame > count:
+		frame = count
+		material.set("shader_parameter/enabled", true)
+		await get_tree().create_timer(0.05, false).timeout
+		material.set("shader_parameter/enabled", false)
+		#flash_white(0.1)
 
-func flash_white():
+func flash_white(time: float):
 	material.set("shader_parameter/enabled", true)
-	flash_white_timer.start()
+	flash_white_timer.start(time)
 
 func on_flash_white_end():
 	material.set("shader_parameter/enabled", false)
 	flash_white_timer.start()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
